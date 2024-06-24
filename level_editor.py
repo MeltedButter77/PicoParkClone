4c3f56b46179618cba8b11d2d52c5e7043f29d57
MeltedButetr77 <MeltedButterGaming@gmail.com>
2024-06-24 12:20:28 +0930

    Added in-game loading and saving levels

--- level_editor.py
+++ level_editor.py
@@ -4,101 +4,29 @@ import utils
 import player
 import json
 import wall
-
-
-class InputBox:
-    def __init__(self, x, y, w, h, text=''):
-        self.active_colour = "blue"
-        self.inactive_colour = "dark blue"
-        self.font = pg.font.Font(None, 32)
-
-        self.rect = pg.Rect(x, y, w, h)
-        self.color = self.inactive_colour
-        self.text = text
-        self.txt_surface = self.font.render(text, True, self.color)
-        self.active = True
-
-    def handle_event(self, event):
-        if event.type == pg.MOUSEBUTTONDOWN:
-            # If the user clicked on the input_box rect.
-            if self.rect.collidepoint(event.pos):
-                # Toggle the active variable.
-                self.active = not self.active
-            else:
-                self.active = False
-            # Change the current color of the input box.
-            self.color = self.active_colour if self.active else self.inactive_colour
-        if event.type == pg.KEYDOWN:
-            if self.active:
-                if event.key == pg.K_RETURN:
-                    return self.text
-                elif event.key == pg.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                else:
-                    self.text += event.unicode
-                # Re-render the text.
-                self.txt_surface = self.font.render(self.text, True, self.color)
-
-    def update(self):
-        # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width() + 10)
-        self.rect.w = width
-
-    def draw(self, screen):
-        # Blit the text.
-        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
-        # Blit the rect.
-        pg.draw.rect(screen, self.color, self.rect, 2)
+import menu
 
 
 class LevelEditor:
     def __init__(self, app):
         self.app = app
-        self.path = "data/levels/level_editor_"
-        self.level = utils.json_load(self.path + "load.json")
 
         self.camera = pg.Vector2(0, 0)
-
-        # prevents any failed level load being erred below
-        if not self.level:
-            return
 
         self.dt = 1
         self.keys_pressed = None
 
+        self.input_box = menu.InputBox(10, 10, 140, 32)
+
         self.players = pg.sprite.Group()
         self.walls = pg.sprite.Group()
 
-        for i, info in enumerate(self.level["players"]):
-            controls = None
-
-            # Player controls should be imported from json, for now they are all stored here
-            if i == 0:
-                controls = [pg.K_w, pg.K_a, pg.K_s, pg.K_d]
-            elif i == 1:
-                controls = [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT]
-            elif i == 2:
-                controls = [pg.K_i, pg.K_j, pg.K_k, pg.K_l]
-
-            if controls:
-                player.Player(self, (info["x"], info["y"]), (info["width"], info["height"]), controls, self.players)
-            else:
-                print("not enough controls defined for player", i + 1)
-        for wall_info in self.level["walls"]:
-            wall.Wall(self, (wall_info["x"], wall_info["y"]), (wall_info["width"], wall_info["height"]), self.walls)
-
     def run(self):
-        # If level is not loaded, return to menu
-        if not self.level:
-            return "menu_main"
         
         camera_mouse_down = None
         selected_wall = None
         mouse_down_pos = None
         display_rect = None
-
-        input_box = None
-        input_result = None
 
         while True:
             for event in pg.event.get():
@@ -106,38 +34,16 @@ class LevelEditor:
                     pg.quit()
                     quit()
 
-                if input_box:
-                    input_result = input_box.handle_event(event)
-                    if input_result:
-                        info_dict = {
-                            "players": [],
-                            "walls": []
-                        }
-                        for wall_obj in self.walls.sprites():
-                            info_dict["walls"].append({
-                                "x": wall_obj.rect.x,
-                                "y": wall_obj.rect.y,
-                                "width": wall_obj.rect.width,
-                                "height": wall_obj.rect.height,
-                            })
-                        for player_obj in self.players.sprites():
-                            info_dict["players"].append({
-                                "x": player_obj.rect.centerx,
-                                "y": player_obj.rect.centery,
-                                "width": player_obj.rect.width,
-                                "height": player_obj.rect.height,
-                            })
-
-                        path = "data/levels/level_" + input_result + ".json"
-                        # Write to json
-                        with open(path, "w") as write:
-                            json.dump(info_dict, write, indent=2)
+                self.input_box.handle_event(event)
 
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_ESCAPE:
                         return "menu_main"
-                    if event.key == pg.K_e:
-                        input_box = InputBox(10, 10, 140, 32)
+                    if not self.input_box.active:
+                        if event.key == pg.K_s and self.input_box.text:
+                            self.export()
+                        if event.key == pg.K_l and self.input_box.text:
+                            self.load()
 
                 # Camera movement
                 if event.type == pg.MOUSEBUTTONDOWN and event.button == 2:
@@ -149,42 +55,45 @@ class LevelEditor:
                         self.camera.x -= event.rel[0]
                         self.camera.y -= event.rel[1]
 
-                # Remove walls
-                if event.type == pg.MOUSEBUTTONDOWN and event.button == 3:
-                    for wall_obj in self.walls:
-                        if wall_obj.rect.move(self.camera * -1).collidepoint(event.pos):
-                            self.walls.remove(wall_obj)
+                # Creation and movement of rects can only happen when the button is not active
+                if not self.input_box.active:
+                    # Remove walls
+                    if event.type == pg.MOUSEBUTTONDOWN and event.button == 3:
+                        for wall_obj in self.walls:
+                            if wall_obj.rect.move(self.camera * -1).collidepoint(event.pos):
+                                self.walls.remove(wall_obj)
 
-                # Create new (set mouse_down_pos) or select to move
-                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-                    for wall_obj in self.walls:
-                        if wall_obj.rect.move(self.camera * -1).collidepoint(event.pos):
-                            selected_wall = wall_obj
-                    if not selected_wall:
-                        mouse_down_pos = (round(event.pos[0], -1), round(event.pos[1], -1))
-                        display_rect = pygame.Rect((round(mouse_down_pos[0], -1), round(mouse_down_pos[1], -1)), (event.pos[0] - mouse_down_pos[0], event.pos[1] - mouse_down_pos[1]))
+                    # Create new (set mouse_down_pos) or select to move
+                    if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                        for wall_obj in self.walls:
+                            if wall_obj.rect.move(self.camera * -1).collidepoint(event.pos):
+                                selected_wall = wall_obj
+                        if not selected_wall:
+                            mouse_down_pos = (round(event.pos[0], -1), round(event.pos[1], -1))
+                            display_rect = pygame.Rect((round(mouse_down_pos[0], -1), round(mouse_down_pos[1], -1)), (event.pos[0] - mouse_down_pos[0], event.pos[1] - mouse_down_pos[1]))
 
-                # Create New walls (complete & display)
-                if event.type == pg.MOUSEBUTTONUP and mouse_down_pos and event.button == 1:
-                    wall.Wall(self, (display_rect.x + self.camera.x, display_rect.y + self.camera.y), (display_rect.width, display_rect.height), self.walls)
-                    mouse_down_pos = None
-                    display_rect = None
-                if event.type == pg.MOUSEMOTION and mouse_down_pos and event.buttons[0]:
-                    # Determine the top-left and bottom-right points
-                    top_left = (round(min(mouse_down_pos[0], event.pos[0]), -1), round(min(mouse_down_pos[1], event.pos[1]), -1))
-                    bottom_right = (round(max(mouse_down_pos[0], event.pos[0]), -1), round(max(mouse_down_pos[1], event.pos[1]), -1))
+                    # Create New walls (complete & display)
+                    if event.type == pg.MOUSEBUTTONUP and mouse_down_pos and event.button == 1:
+                        if display_rect.width > 0 and display_rect.height > 0:
+                            wall.Wall(self, (display_rect.x + self.camera.x, display_rect.y + self.camera.y), (display_rect.width, display_rect.height), self.walls)
+                        mouse_down_pos = None
+                        display_rect = None
+                    if event.type == pg.MOUSEMOTION and mouse_down_pos and event.buttons[0]:
+                        # Determine the top-left and bottom-right points
+                        top_left = (round(min(mouse_down_pos[0], event.pos[0]), -1), round(min(mouse_down_pos[1], event.pos[1]), -1))
+                        bottom_right = (round(max(mouse_down_pos[0], event.pos[0]), -1), round(max(mouse_down_pos[1], event.pos[1]), -1))
 
-                    # Calculate width and height
-                    width = bottom_right[0] - top_left[0]
-                    height = bottom_right[1] - top_left[1]
+                        # Calculate width and height
+                        width = bottom_right[0] - top_left[0]
+                        height = bottom_right[1] - top_left[1]
 
-                    display_rect = pygame.Rect(top_left, (width, height))
+                        display_rect = pygame.Rect(top_left, (width, height))
 
-                # Move walls (complete & move display)
-                if event.type == pg.MOUSEMOTION and selected_wall:
-                    selected_wall.rect.center = event.pos + self.camera
-                if event.type == pg.MOUSEBUTTONUP and selected_wall:
-                    selected_wall = None
+                    # Move walls (complete & move display)
+                    if event.type == pg.MOUSEMOTION and selected_wall:
+                        selected_wall.rect.center = event.pos + self.camera
+                    if event.type == pg.MOUSEBUTTONUP and selected_wall:
+                        selected_wall = None
 
             ### LOGIC ###
             self.keys_pressed = pg.key.get_pressed()
@@ -203,14 +112,74 @@ class LevelEditor:
             if display_rect:
                 pygame.draw.rect(self.app.screen, (255, 255, 255), display_rect)
 
-            if input_box:
-                input_box.update()
-                input_box.draw(self.app.screen)
+            self.input_box.draw(self.app.screen)
 
             pg.display.update()
             self.app.screen.fill((60, 60, 60))
             self.dt = self.app.clock.tick(self.app.fps) / 1000
             pg.display.set_caption("FPS: " + str(int(self.app.clock.get_fps())))
+
+    def export(self):
+        info_dict = {
+            "players": [],
+            "walls": []
+        }
+        for wall_obj in self.walls.sprites():
+            info_dict["walls"].append({
+                "x": wall_obj.rect.x,
+                "y": wall_obj.rect.y,
+                "width": wall_obj.rect.width,
+                "height": wall_obj.rect.height,
+            })
+        for player_obj in self.players.sprites():
+            info_dict["players"].append({
+                "x": player_obj.rect.centerx,
+                "y": player_obj.rect.centery,
+                "width": player_obj.rect.width,
+                "height": player_obj.rect.height,
+            })
+
+        path = f"data/levels/level_{self.input_box.text}.json"
+        # Write to json
+        with open(path, "w") as write:
+            json.dump(info_dict, write, indent=2)
+
+    def load(self):
+        # Clear current sprites
+        self.players = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+
+        path = f"data/levels/level_{self.input_box.text}.json"
+        level_info = utils.json_load(path)
+
+        if not level_info:
+            print("failed to load level")
+            return
+
+        if level_info["players"]:
+            for i, info in enumerate(level_info["players"]):
+                controls = None
+
+                # Player controls should be imported from json, for now they are all stored here
+                if i == 0:
+                    controls = [pg.K_w, pg.K_a, pg.K_s, pg.K_d]
+                elif i == 1:
+                    controls = [pg.K_UP, pg.K_LEFT, pg.K_DOWN, pg.K_RIGHT]
+                elif i == 2:
+                    controls = [pg.K_i, pg.K_j, pg.K_k, pg.K_l]
+
+                if controls:
+                    player.Player(self, (info["x"], info["y"]), (info["width"], info["height"]), controls, self.players)
+                else:
+                    print("not enough controls defined for player", i + 1)
+        else:
+            print("no players in level")
+
+        if level_info["walls"]:
+            for wall_info in level_info["walls"]:
+                wall.Wall(self, (wall_info["x"], wall_info["y"]), (wall_info["width"], wall_info["height"]), self.walls)
+        else:
+            print("no walls in level")
 
     def move_camera(self, keys_pressed):
         if keys_pressed[pg.K_LEFT]:
--- menu.py
+++ menu.py
@@ -1,13 +1,60 @@
-import pygame as py
+import pygame as pg
 import utils
+
+
+class InputBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.active_colour = "blue"
+        self.inactive_colour = "dark blue"
+        self.font = pg.font.Font(None, 32)
+
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = self.inactive_colour
+        self.text = text
+        self.txt_surface = self.font.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = True
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = self.active_colour if self.active else self.inactive_colour
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    self.active = False
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = self.font.render(self.text, True, self.color)
+            self.color = self.active_colour if self.active else self.inactive_colour
+            self.update()
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(self.rect.width, self.txt_surface.get_width() + 10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
 
 
 class Button:
     def __init__(self, screen, center_x, center_y, width, height, border, curve, buttonColour, textColour, hoverColour, id, text, font='freesansbold.ttf', font_size=80, text_offset=0):
-        py.init()
-        py.font.init()
+        pg.init()
+        pg.font.init()
         self.font_size = font_size
-        self.font = py.font.Font(font, font_size)
+        self.font = pg.font.Font(font, font_size)
         self.text_offset = text_offset
 
         self.screen = screen
@@ -25,7 +72,7 @@ class Button:
         self.hover_colour = hoverColour
         self.id = id
         self.text = text
-        self.rect = py.Rect(self.x, self.y, self.width, self.height)
+        self.rect = pg.Rect(self.x, self.y, self.width, self.height)
 
         self.highlighted = False
 
@@ -35,16 +82,16 @@ class Button:
 
         if self.rect.collidepoint(event.pos):
             self.highlighted = True
-            if event.type == py.MOUSEBUTTONDOWN:
+            if event.type == pg.MOUSEBUTTONDOWN:
                 return True
         elif not self.rect.collidepoint(event.pos):
             self.highlighted = False
 
     def draw(self):
         if self.highlighted:
-            py.draw.rect(self.screen, self.hover_colour, self.rect, self.border, self.curve)
+            pg.draw.rect(self.screen, self.hover_colour, self.rect, self.border, self.curve)
         else:
-            py.draw.rect(self.screen, self.buttonColour, self.rect, self.border, self.curve)
+            pg.draw.rect(self.screen, self.buttonColour, self.rect, self.border, self.curve)
 
         if self.text != "":
             text_surf = self.font.render(self.text, True, self.textColour)
@@ -70,13 +117,13 @@ class Menu:
             return "menu_main"
 
         while True:
-            for event in py.event.get():
-                if event.type == py.QUIT:
-                    py.quit()
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
                     quit()
-                if event.type == py.KEYDOWN:
-                    if event.key == py.K_ESCAPE:
-                        py.quit()
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        pg.quit()
                         quit()
 
                 for button in self.buttons:
@@ -86,7 +133,7 @@ class Menu:
             for button in self.buttons:
                 button.draw()
 
-            py.display.update()
+            pg.display.update()
             self.app.screen.fill((60, 60, 60))
             self.app.clock.tick(self.app.fps)
-            py.display.set_caption("FPS: " + str(int(self.app.clock.get_fps())))
+            pg.display.set_caption("FPS: " + str(int(self.app.clock.get_fps())))
